912bae0d8dd0bd47c10993d52c4b95f8899880e6
Somanathkl <lsomanath@deloitte.com>
2023-12-18 19:29:35 +0530

    Added Deployment Files

--- scripts/shellScripts/commitExtractPR.sh
+++ scripts/shellScripts/commitExtractPR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd SourceBranchPath
 
-if [[ $? -ne 0 ]]
+if [ $? -ne 0 ];
 then
     echo "Source Branch Path Not Found!!"
     exit 1
@@ -12,13 +12,12 @@ echo "Source Branch Commit ID = $SOURCE_COMMIT_ID"
 echo "SOURCE_COMMIT_ID=$SOURCE_COMMIT_ID" >> $GITHUB_ENV
 
 cd ../TargetBranchPath
-if [[ $? -ne 0 ]]
+if [ $? -ne 0 ];
 then
     echo "Target Branch Path Not Found!!"
     exit 1
 fi
 
-#TARGET_COMMIT_ID=$(git log | grep commit | head -1 | tail -1 | awk '{print $2}')
-TARGET_COMMIT_ID='8cdd804335bd1ad7c9840793f13b77b050bcd410'
+TARGET_COMMIT_ID=$(git log | grep commit | head -1 | tail -1 | awk '{print $2}')
 echo "Target Branch Commit ID = $TARGET_COMMIT_ID"
 echo "TARGET_COMMIT_ID=$TARGET_COMMIT_ID" >> $GITHUB_ENV
